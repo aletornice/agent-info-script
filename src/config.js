c5163b958081867bf260d2b2b80fbd8a9d9987f1
aletornice <aletornice@hotmail.com>
2024-05-07 14:16:13 -0300

    Update config.js Id Client Vivi

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const configuration = {
     regions: {
       stage: "dev",
       name: "mypurecloud.com",
-      clientId: "e12e8150-65a8-4b4f-ac16-f914e2559c03",
+      clientId: "1a663d09-215e-4e3f-bd91-ea9de4d0eacb",
       appId: "insert-agent-message"
     },
   },
